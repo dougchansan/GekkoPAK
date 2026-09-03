62ae4e8e8e729b38a64f405c22e7754d33918881
dougchansan <douglasw808@gmail.com>
2026-09-03 01:32:40 -1000

    Run executable NTR v1 model regression

--- prototype/azahar-ntr/scripts/run_model.sh
+++ prototype/azahar-ntr/scripts/run_model.sh
@@ -13,3 +13,8 @@ c++ -std=c++17 -O2 -Wall -Wextra -Wpedantic -Werror \
   "$HERE/model/ntr_v1_budget.cpp" \
   -o "$BUILD/ntr_v1_budget"
 "$BUILD/ntr_v1_budget"
+
+c++ -std=c++17 -O2 -Wall -Wextra -Wpedantic -Werror \
+  "$HERE/model/gekkopak_ntr_model.cpp" "$HERE/model/ntr_v1_model_test.cpp" \
+  -o "$BUILD/ntr_v1_model_test"
+"$BUILD/ntr_v1_model_test"
